821b5864d5c8f66052b47dab2498eb9ad7b56b0c
hidzz1 <156692237+hidzz1@users.noreply.github.com>
2024-02-20 16:53:36 +0000

    Create Replace.py

---
+++ Replace.py
@@ -0,0 +1,11 @@
+a="The!quick!brown!fox!jumps!over!the!lazy!dog."
+print (a.replace("!", " "))
+#prints out The quick brown fox jumps over the lazy dog.
+
+a_string="The quick brown fox jumps over the lazy dog."
+print(a_string.upper())
+#prints out THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+a_string2="THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+position=a_string2[::-1]
+print(position)
